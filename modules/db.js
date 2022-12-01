578af2c8fd01179a8f197356633ef3956e7f4e79
abdelilahzaidi <abdelilahzaidi1983@gmail.com>
2022-12-01 11:57:38 +0100

    create a fake db

---
+++ modules/db.js
@@ -0,0 +1,16 @@
+const db=[
+    user={
+        id:1,
+        firstName:'Ab',
+        lastName:'Zaidi',
+        email:'abdelilahzaidi1983@gmail.com',
+        passwd:'14101983'
+    },
+    {
+        id:2,
+        firstName:'Miyamoto',
+        lastName:'Musashi',
+        email:'musashi@gmail.com',
+        passwd:'14101983'
+    }
+]
